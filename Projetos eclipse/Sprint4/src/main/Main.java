5fc04c6cbbd1aab273718be1be71fd43c008b7eb
Gabriel <gabrielguedessilva.guedes51@gmail.com>
2022-10-07 17:27:48 -0300

    commit 10/7/22 - testando o repositório

--- Projetos eclipse/Sprint4/src/main/Main.java
+++ Projetos eclipse/Sprint4/src/main/Main.java
@@ -4,8 +4,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Relatorio rl = new Relatorio();
-		// (número de testes, números de elementos por teste, número de buscas por teste)
-		rl.gerarRelatorio(30, 1000, 300);
+		// (numero de testes, numeros de elementos por teste, numero de buscas por teste)
+		//rl.gerarRelatorio(30, 1000, 300);
+		rl.gerarRelatorio(45, 3000, 450);
 	}
 
 }
